334945956de3cfebb7e41060e0499e665824cdc7
checkr <ugochiori@gmail.com>
2026-09-08 11:01:12 +0100

    deps installed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center  ">
+      <h1 className="text-white">hello </h1>
+      
+       
+    </div>
+  )
+}
+
+export default App
