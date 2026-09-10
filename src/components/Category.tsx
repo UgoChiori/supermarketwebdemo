7403898fe254cbc5ac177875bd905bbfcdeb5bb9
checkr <ugochiori@gmail.com>
2026-09-10 09:13:43 +0100

    json added

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 // import { images } from '../assets/images';
-
+import productsData from "../assets/supermarketassetmap.json"
 
 
 
 const Category: React.FC = () => {
+   console.log(productsData);
     // const [activeTab, setActiveTab] = useState('tab1');
   return (
     <div>Category</div>
